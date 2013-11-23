018812492405cb00840fc0c26758e9a9f2fdbb17
snusnu <gamsnjaga@gmail.com>
2013-11-23 20:53:40 +0100

    Simplify AST::Builder::Relation::Base

--- lib/axiom/schema/ast/builder/relation/base.rb
+++ lib/axiom/schema/ast/builder/relation/base.rb
@@ -27,31 +27,31 @@ module Axiom
             end
 
             def add_attribute(*args)
-              update([attributes << s(:attribute, *args)] + children[1..-1])
+              update([attributes << s(:attribute, *args)] + children.drop(1))
             end
 
             def add_key_constraint(header)
-              update([attributes, key_constraints << s(:key_constraint, *header)] + children[2..-1])
+              update([attributes, key_constraints << s(:key_constraint, *header)] + children.drop(2))
             end
 
             def add_fk_constraint(header)
-              update([attributes, key_constraints, fk_constraints << s(:fk_constraint, *header)] + children[3..-1])
+              update([attributes, key_constraints, fk_constraints << s(:fk_constraint, *header)] + children.drop(3))
             end
 
             def with_database(name)
-              update((children[0..3] << database.updated(nil, [name])) + children[5..-1])
+              update((children.take(4) << database.updated(nil, [name])) + children.drop(5))
             end
 
             def with_pk_constraint(header)
-              update((children[0..2] << pk_constraint.updated(nil, header)) + children[4..-1])
+              update((children.take(3) << pk_constraint.updated(nil, header)) + children.drop(4))
             end
 
             def with_aliases(new_aliases)
-              update(children[0..4] << aliases.updated(nil, [new_aliases]) << name)
+              update(children.take(5) << aliases.updated(nil, [new_aliases]) << name)
             end
 
             def with_name(new_name)
-              update(children[0..-2] << name.updated(nil, [new_name]))
+              update(children.take(6) << name.updated(nil, [new_name]))
             end
 
           end # Base
